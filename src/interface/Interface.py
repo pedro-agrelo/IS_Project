4e51f39f17d078661918313f521c3bd878522ebf
Diego-Lopez-Gonzalez <diego.lopez.gonzalez@udc.es>
2024-10-24 16:28:30 +0200

    refactorización

--- src/interface/Interface.py
+++ src/interface/Interface.py
@@ -43,14 +43,6 @@ class FileExplorerInterface(QMainWindow):
         self.column_selector = ColumnSelector(self)
         layout.addWidget(self.column_selector)
 
-        # Botón para confirmar selección (No visible al principio)
-        self.confirm_button = QPushButton('Confirm columns selection')
-        self.confirm_button.setFixedSize(300, 50)
-        self.confirm_button.setVisible(False)  # Ocultar el botón al inicio
-        self.confirm_button.setFont(QFont("Arial", 16, QFont.Bold))  # Aumentar tamaño de letra
-        self.confirm_button.clicked.connect(self.confirmar_seleccion)
-        layout.addWidget(self.confirm_button, alignment=Qt.AlignCenter)
-
         # Contenedor central
         container = QWidget()
         container.setLayout(layout)
@@ -68,82 +60,18 @@ class FileExplorerInterface(QMainWindow):
                 headers = [self.table_widget.horizontalHeaderItem(i).text() for i in range(self.table_widget.columnCount())]
                 self.column_selector.update_selectors(headers)
                 self.column_selector.setVisible(True)
-                self.confirm_button.setVisible(True)  # Mostrar el botón de confirmación
+                self.column_selector.confirm_button.setVisible(True)  # Mostrar el botón de confirmación
             else:
                 self.column_selector.setVisible(False)
-                self.confirm_button.setVisible(False)  # Ocultar el botón si no se carga el archivo
+                self.column_selector.confirm_button.setVisible(False)  # Ocultar el botón si no se carga el archivo
                 self.show_empty_file_message()
         else:
             self.table_widget.setVisible(False)
             self.column_selector.setVisible(False)
-            self.confirm_button.setVisible(False)  # Ocultar el botón si no hay archivo
+            self.column_selector.confirm_button.setVisible(False)  # Ocultar el botón si no hay archivo
             self.label.setText("<b>No file selected</b>")
             self.label.setStyleSheet("color: #FF6347;")
 
-    def confirmar_seleccion(self):
-        """Confirma la selección de columnas de entrada y salida"""
-        entradas, salida = self.column_selector.get_selected_columns()
-
-        if not entradas or not salida:
-            self.mostrar_mensaje("Error", "Debe seleccionar al menos una columna de entrada y una columna de salida.", "warning")
-        else:
-            self.mostrar_mensaje("Success", f"Selected columns:\nEnter: {entradas}\nExit: {salida}", "success")
-
-    def mostrar_mensaje(self, titulo, mensaje, tipo):
-        """Muestra un cuadro de diálogo con el estilo adecuado"""
-        msg_box = QMessageBox(self)
-
-        # Ajustar estilo según el tipo de mensaje
-        if tipo == "warning":
-            msg_box.setIcon(QMessageBox.Warning)
-            msg_box.setStyleSheet("""
-                QMessageBox {
-                    background-color: #333;
-                    color: white;
-                }
-                QLabel {
-                    font-size: 14px;  /* Aumentar el tamaño de la letra */
-                    color: white;
-                }
-                QPushButton {
-                    background-color: #555;
-                    color: white;
-                    border: 2px solid #FFFFFF;
-                    border-radius: 8px;
-                    padding: 10px;
-                    font-size: 14px;  /* Aumentar el tamaño de la letra */
-                }
-                QPushButton:hover {
-                    background-color: #777;
-                }
-            """)
-        elif tipo == "success":
-            msg_box.setIcon(QMessageBox.Information)
-            msg_box.setStyleSheet("""
-                QMessageBox {
-                    background-color: #333;
-                    color: white;
-                }
-                QLabel {
-                    font-size: 14px;  /* Aumentar el tamaño de la letra */
-                    color: white;
-                }
-                QPushButton {
-                    background-color: #0078d4;
-                    color: white;
-                    border: 2px solid #FFFFFF;
-                    border-radius: 8px;
-                    padding: 10px;
-                    font-size: 14px;  /* Aumentar el tamaño de la letra */
-                }
-                QPushButton:hover {
-                    background-color: #005A9E;
-                }
-            """)
-
-        msg_box.setWindowTitle(titulo)
-        msg_box.setText(mensaje)
-        msg_box.exec_()
 
     def apply_styles(self):
         """Aplica estilos (QSS) a los widgets"""
@@ -161,7 +89,6 @@ class FileExplorerInterface(QMainWindow):
             }
         """
         self.button.setStyleSheet(button_style)  # Botón de abrir archivos
-        self.confirm_button.setStyleSheet(button_style)  # Botón de confirmar selección
 
         # Estilos de la ventana y paleta de colores
         palette = QPalette()
