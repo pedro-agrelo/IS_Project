15e9e425b5a41d5f3d3a2ad6b69d25eeca59aaaa
pedroagrelo <pedro.agrelo.marquez@gmail.com>
2024-10-24 13:02:03 +0200

    comentario

--- src/linear_models/statsmodelstest.py
+++ src/linear_models/statsmodelstest.py
@@ -5,9 +5,9 @@ import statsmodels.formula.api as sm
 df=pd.read_csv(r'C:/Users/pedro/OneDrive/Documentos/CLASE/2º/SOFTWARE/Proyecto/IS_Project/src/linear_models/housing.csv')
 print(df.head())
 
-regresion = sm.ols('total_bedrooms ~ population - households', data=df)
+regresion = sm.ols('total_bedrooms ~ population - households', data=df) #Crear modelo linear
 sol=regresion.fit() #Ajuste
 
 print(sol.summary())
-print('R**2 = ', sol.rsquared)
+print('R**2 = ', sol.rsquared
 print(sol.params)
